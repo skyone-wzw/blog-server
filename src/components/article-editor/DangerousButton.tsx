5ffb83903d1c6afab8de8383585dc474611e87a7
skyone-wzw <master@skyone.dev>
2024-01-19 16:29:54 +0800

    为删除文章添加防误触

--- src/components/article-editor/DangerousButton.tsx
+++ src/components/article-editor/DangerousButton.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import clsx from "clsx";
 import {ReactNode, useEffect, useRef, useState} from "react";
 
 interface DangerousButtonProps {
@@ -10,19 +11,19 @@ interface DangerousButtonProps {
 
 function DangerousButton({className, children, onClick}: DangerousButtonProps) {
     const ref = useRef<HTMLButtonElement>(null);
-    const [isConfirm, setIsConfirm] = useState(false);
+    const [isConfirm, setIsConfirm] = useState(0);
 
     useEffect(() => {
         if (isConfirm) {
             // 如果 3 秒内没有点击按钮，就自动取消
             const timer = setTimeout(() => {
-                setIsConfirm(false);
+                setIsConfirm(0);
             }, 3000);
 
             // 如果点击了按钮之外的地方，就自动取消
             const handleClick = (event: MouseEvent) => {
                 if (ref.current && !ref.current.contains(event.target as Node)) {
-                    setIsConfirm(false);
+                    setIsConfirm(0);
                 }
             };
             window.addEventListener("click", handleClick);
@@ -36,18 +37,24 @@ function DangerousButton({className, children, onClick}: DangerousButtonProps) {
 
     const handleClick = () => {
         if (isConfirm) {
+            if (Date.now() - isConfirm < 200) {
+                // 点击间隔小于 200ms, 可能为误触, 不执行
+                return;
+            }
             onClick();
-            setIsConfirm(false);
+            setIsConfirm(0);
         } else {
-            setIsConfirm(true);
+            setIsConfirm(Date.now());
         }
     };
 
     return (
         <button
-            className={className}
+            className={clsx(className, {
+                "bg-red-500 hover:bg-red-600": isConfirm !== 0,
+            })}
             onClick={handleClick} ref={ref}>
-            {isConfirm ? "确认吗？" : children}
+            {isConfirm !== 0 ? "确认吗？" : children}
         </button>
     );
 }
